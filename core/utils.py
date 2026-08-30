43d67722a28993705fc5dee59e099cde3aca7dcf
succes-klassx <succes@reussir-mon-bac.com>
2026-08-30 10:02:18 -0400

    Clean up old txt file and push proper utils.py

--- core/utils.py
+++ core/utils.py
@@ -5,19 +5,19 @@ from django.conf import settings
 def send_brevo_email(to_email, subject, html_content):
     api_key = os.getenv('BREVO_API_KEY')
     url = "https://api.brevo.com/v3/smtp/email"
-    
+
     headers = {
         "accept": "application/json",
         "api-key": api_key,
         "content-type": "application/json"
     }
-    
+
     payload = {
         "sender": {"email": "contrat@klassx.cloud", "name": "KLASSX"},
         "to": [{"email": to_email}],
         "subject": subject,
         "htmlContent": html_content
     }
-    
+
     response = requests.post(url, json=payload, headers=headers)
     return response.status_code == 201
