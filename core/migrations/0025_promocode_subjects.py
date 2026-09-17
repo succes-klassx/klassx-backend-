2daa603f5d6ae1ee438573ae2b7b20b1a7db9a8e
succes-klassx <succes@reussir-mon-bac.com>
2026-09-17 10:47:27 -0400

    Suppression doublon migration

--- core/migrations/0025_promocode_subjects.py
+++ core/migrations/0025_promocode_subjects.py
@@ -1,18 +1,6 @@
 from django.db import migrations, models
 
 
-def copy_subject_to_subjects(apps, schema_editor):
-    """Reprend l'ancien lien simple (subject) dans le nouveau champ multiple (subjects), pour ne perdre aucun code promo existant."""
-    PromoCode = apps.get_model("core", "PromoCode")
-    for promo in PromoCode.objects.exclude(subject__isnull=True):
-        promo.subjects.add(promo.subject_id)
-
-
-def noop_reverse(apps, schema_editor):
-    """Pas de retour en arrière automatique : un code promo pourrait avoir plusieurs matières une fois la migration appliquée, ce qui ne rentre plus dans l'ancien champ à une seule matière."""
-    pass
-
-
 class Migration(migrations.Migration):
 
     dependencies = [
@@ -25,7 +13,6 @@ class Migration(migrations.Migration):
             name="subjects",
             field=models.ManyToManyField(blank=True, to="core.subject"),
         ),
-        migrations.RunPython(copy_subject_to_subjects, noop_reverse),
         migrations.RemoveField(
             model_name="promocode",
             name="subject",
